244afdfd3ace2ec8a109d0cfadbc161c7dd37368
SH2G <suhy427@gmail.com>
2024-04-12 18:21:07 +0900

    Feat: DB연결

--- database/entity/history.go
+++ database/entity/history.go
@@ -6,11 +6,11 @@ import (
 
 type Event struct {
 	gorm.Model
-	Name    string `gorm:"column:name; not null"`
-	Level   string `gorm:"column:level; not null"`
-	IsUsed  bool   `gorm:"column:is_used; not null"`
+	Name    string `gorm:"column:name;not null"`
+	Level   string `gorm:"column:level;not null"`
+	IsUsed  bool   `gorm:"column:is_used;not null"`
 	Detail  Detail
-	Userdex Userdex
+	Userdex UserDex
 	Mapping Mapping
 	Comment Comment
 }
@@ -21,34 +21,31 @@ func (Event) TableName() string {
 
 type Detail struct {
 	gorm.Model
-	DexId      int    `gorm:"column:dex_id; foreignKey:EventId; not null"`
+	EventId    int    `gorm:"column:dex_id;foreignKey:EventId;"`
 	Define     string `gorm:"column:define"`
 	Outline    string `gorm:"column:outline"`
 	Place      string `gorm:"column:place"`
 	Background string `gorm:"column:background"`
-	// Passing    string `gorm:"type:varchar"`
-	// Outcome    string `gorm:"type:varchar"`
-	// Assessment string `gorm:"type:varchar"`
-	ImageUrl string `gorm:"column:image_url"`
+	ImageUrl   string `gorm:"column:image_url"`
 }
 
 func (Detail) TableName() string {
 	return "detail"
 }
 
-type Userdex struct {
+type UserDex struct {
 	gorm.Model
-	DexId  int `gorm:"column:dex_id; foreignKey:Id; not null"`
-	UserId int `gorm:"column:user_id; not null"`
+	EventId int `gorm:"column:dex_id; foreignKey:EventId;"`
+	UserId  int `gorm:"column:user_id;not null"`
 }
 
-func (Userdex) TableName() string {
+func (UserDex) TableName() string {
 	return "userdex"
 }
 
 type Tag struct {
 	gorm.Model
-	Name    string `gorm:"column:name; not null"`
+	Name    string `gorm:"column:name;not null"`
 	Mapping Mapping
 }
 
@@ -57,9 +54,9 @@ func (Tag) TableName() string {
 }
 
 type Mapping struct {
-	Id    int `gorm:"primaryKey; column:id"`
-	DexId int `gorm:"column:dex_id; foreignKey:EventId; not null"`
-	TagId int `gorm:"column:tag_id; foreignKey:TagId; not null"`
+	Id      int `gorm:"primaryKey;column:id"`
+	EventId int `gorm:"column:dex_id;foreignKey:EventId;"`
+	TagId   int `gorm:"column:tag_id;foreignKey:TagId;"`
 }
 
 func (Mapping) TableName() string {
@@ -68,9 +65,9 @@ func (Mapping) TableName() string {
 
 type Comment struct {
 	gorm.Model
-	DexId   int    `gorm:"column:dex_id; foreignKey:EventId; not null"`
-	UserId  int    `gorm:"column:user_id; not null"`
-	Content string `gorm:"column:content; not null"`
+	EventId int    `gorm:"column:dex_id;foreignKey:EventId;"`
+	UserId  int    `gorm:"column:user_id;not null"`
+	Content string `gorm:"column:content;not null"`
 }
 
 func (Comment) TableName() string {
@@ -79,10 +76,8 @@ func (Comment) TableName() string {
 
 type Quote struct {
 	gorm.Model
-	Content  string `gorm:"column:content; not null"`
-	ImageUrl string `gorm:"column:image_url; not null"`
-	StartAt  string `gorm:"column:start_at; not null"`
-	EndAt    string `gorm:"column:end_at; not null"`
+	Content  string `gorm:"column:content;not null"`
+	ImageUrl string `gorm:"column:image_url;not null"`
 }
 
 func (Quote) TableName() string {
