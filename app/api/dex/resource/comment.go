c8bde355a9ef52b6e3ac2f3c1b15e19d48b51c0a
WGCAT <seed9878@naver.com>
2024-05-03 15:35:23 +0900

    (docs): comment / dex 브랜치 분할 - comment 브랜치에서의 dex 내용 삭제

--- app/api/dex/resource/comment.go
+++
@@ -1,9 +0,0 @@
-package resource
-
-// dex
-type DexEventResource struct {
-	Id       uint64 `json:"id"`
-	UserId   uint64 `json:"user_id"`
-	UserName string `json:"nickname"`
-	Content  string `json:"content"`
-}
