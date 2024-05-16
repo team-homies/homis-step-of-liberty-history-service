a16e31a49ad20ac73af21f2b8f54057dd3d4fc9f
SH2G <suhy427@gmail.com>
2024-05-17 04:21:54 +0900

    (feat): 도감 필터 조회 service 추가

---
+++ app/api/dex/service/service.go
@@ -0,0 +1,36 @@
+package service
+
+import (
+	"main/app/api/dex/resource"
+	"main/database/repository"
+)
+
+type DexService interface {
+	GetTags() (res []resource.GetTagsResponse, err error)
+}
+
+func NewDexService() DexService {
+	return &dexService{
+		DexService: &dexService{},
+	}
+}
+
+type dexService struct {
+	DexService
+}
+
+func (p *dexService) GetTags() (res []resource.GetTagsResponse, err error) {
+	res = []resource.GetTagsResponse{}
+	tags, err := repository.NewRepository().GetTags()
+	if err != nil {
+		return
+	}
+
+	for _, tag := range tags {
+		res = append(res, resource.GetTagsResponse{
+			Id:   tag.ID,
+			Name: tag.Name,
+		})
+	}
+	return res, err
+}
