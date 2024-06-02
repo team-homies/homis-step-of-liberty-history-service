74492a36aa864db9b64128d626dcd35a16fc41fd
SH2G <suhy427@gmail.com>
2024-06-02 16:28:32 +0900

    (refactor): 충돌해결

--- app/api/dex/dex.go
+++ app/api/dex/dex.go
@@ -4,6 +4,7 @@ import (
 	"main/app/api/dex/handler"
 	"main/constant"
 	"main/middleware"
+
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -16,7 +17,6 @@ func SetDexApis(route fiber.Router) {
 	route.Get(constant.DexPath().Dex.GetTags, h.GetTags)
 	// 사용자 사건 수집 등록
 	route.Post(constant.DexPath().Dex.CreateDexEvent, middleware.AuthVerificationMiddleware, h.CreateDexEvent)
-	route.Get(constant.GetPath().Dex.GetTags, h.GetTags)
 	// 명언 조회
-	route.Get(constant.GetPath().Dex.GetQuote, h.GetQuote)
+	route.Get(constant.DexPath().Dex.GetQuote, h.GetQuote)
 }
